e75ed34c349d48884920cb0b792f0df7f9fd0beb
Vikashartist <vikashartist07@gmail.com>
2024-07-24 23:54:10 +0530

    bulb-test 1

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+ const bulbPlc = document.getElementById('bulb-img-plc');
+ const btnOn = document.getElementById('bulb-on');
+ const btnOf = document.getElementById('bulb-off');
+
+ btnOn.addEventListener('click', () => {
+   bulbPlc.src = "/pic_bulbon (1).gif";
+ });
+
+ btnOf.addEventListener('click', () => {
+   bulbPlc.src = "/pic_bulboff (1).gif";
+ });
